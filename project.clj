17797edf03528b0e743452d448ca87dbe1d01923
Emlyn Corrin <Emlyn.Corrin@microsoft.com>
2017-10-23 18:27:46 +0100

    Add cloverage for test coverage

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :description "Floating point comparisons for tests"
   :url "https://github.com/Microsoft/same-ish"
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:plugins [[venantius/ultra "0.5.1"]]}}
+  :profiles {:dev {:plugins [[venantius/ultra "0.5.1" :exclusions [org.clojure/clojure]]
+                             [lein-cloverage "1.0.9" :exclusions [org.clojure/clojure]]]}}
   :test-selectors {:default (complement (some-fn :slow :fail))
                    :most    (complement :fail)
                    :slow    :slow

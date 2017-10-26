55dcf49ff7ab9b68bac20250e51bf5d6c68f68a1
Emlyn Corrin <Emlyn.Corrin@microsoft.com>
2017-10-26 11:08:32 +0100

    Clean up project file, move Clojure to dev dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,14 @@
 (defproject same/ish "0.1.1-SNAPSHOT"
-  :license {:name "MIT License"}
-  :description "Floating point comparisons for tests"
+  :description "A Clojure library for approximate comparison of floating point numbers in tests."
   :url "https://github.com/Microsoft/same-ish"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:plugins [[venantius/ultra "0.5.1" :exclusions [org.clojure/clojure]]
-                             [lein-cloverage "1.0.9" :exclusions [org.clojure/clojure]]]}}
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies []
+  :profiles
+  {:dev
+   {:dependencies [[org.clojure/clojure "1.8.0"]]}}
+  :plugins [[venantius/ultra "0.5.1" :exclusions [org.clojure/clojure]]
+            [lein-cloverage "1.0.9" :exclusions [org.clojure/clojure]]]
   :test-selectors {:default (complement (some-fn :slow :fail))
                    :most    (complement :fail)
                    :slow    :slow

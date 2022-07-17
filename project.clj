633d5e506b93f009d16f4520bc76c12c35d51b14
Emlyn Corrin <emlyn.corrin@gmail.com>
2022-07-17 01:12:17 +0100

    Version 0.1.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject same/ish "0.1.5-SNAPSHOT"
+(defproject same/ish "0.1.5"
   :description "A Clojure library for approximate comparison of floating point numbers in tests."
 
   :license {:name "MIT License"

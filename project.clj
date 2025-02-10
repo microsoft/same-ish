02644717fcfb4f05084e86190fb8582549a8ad27
Emlyn Corrin <emlyn.corrin@gmail.com>
2025-02-10 17:51:04 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -15,12 +15,12 @@
 
   :profiles
   {:dev
-   {:dependencies [[org.clojure/clojure "1.11.1"]
-                   [org.clojure/clojurescript "1.11.60"]
-                   [com.bhauman/figwheel-main "0.2.18"]
-                   [org.slf4j/slf4j-nop "2.0.6"]
-                   [com.bhauman/rebel-readline-cljs "0.1.4"]
-                   [org.clojure/core.rrb-vector "0.1.2"]]
+   {:dependencies [[org.clojure/clojure "1.12.0"]
+                   [org.clojure/clojurescript "1.11.132"]
+                   [com.bhauman/figwheel-main "0.2.20"]
+                   [org.slf4j/slf4j-nop "2.0.16"]
+                   [com.bhauman/rebel-readline-cljs "0.1.5"]
+                   #_[org.clojure/core.rrb-vector "0.2.0"]]
     :resource-paths ["target"]
     ;; need to add the compiled assets to the :clean-targets
     :clean-targets ^{:protect false} ["target"]}
@@ -28,10 +28,11 @@
    :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
    :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
    :1.10 {:dependencies [[org.clojure/clojure "1.10.3"]]}
-   :1.11 {:dependencies [[org.clojure/clojure "1.11.1"]]}
-   :clj-kondo {:dependencies [[clj-kondo "2023.01.20"]
-                              [com.fasterxml.jackson.core/jackson-core "2.14.1"]]}}
+   :1.11 {:dependencies [[org.clojure/clojure "1.11.4"]]}
+   :1.12 {:dependencies [[org.clojure/clojure "1.12.0"]]}
    :antq {:dependencies [[com.github.liquidz/antq "RELEASE"]]}
+   :clj-kondo {:dependencies [[clj-kondo "2025.01.16"]
+                              [com.fasterxml.jackson.core/jackson-core "2.18.2"]]}}
 
   :plugins [;; Nice test output
             #_[venantius/ultra "0.6.0" :exclusions [org.clojure/core.rrb-vector]]
@@ -41,13 +42,13 @@
             [lein-cloverage "1.2.4"]
 
             ;; Code/style checks
-            [lein-cljfmt "0.8.2"]]
+            [lein-cljfmt "0.9.2"]]
 
   ;;:middleware [ultra.plugin/middleware]
 
   :aliases {"checks" ["do" "check" ["cljfmt" "check"] "clj-kondo"]
             "clj-kondo" ["with-profile" "+clj-kondo" "run" "-m" "clj-kondo.main" "--lint" "src" "test"]
-            "tests" ["with-profile" "+1.11:+1.10:+1.9:+1.8:+1.7" "test"]
+            "tests" ["with-profile" "+1.12:+1.11:+1.10:+1.9:+1.8:+1.7" "test"]
             "outdated"       ["with-profile" "antq" "run" "-m" "antq.core"]
             "fig:build"      ["trampoline" "run" "-m" "figwheel.main" "--build" "dev" "--repl"]
             "fig:test"       ["run" "-m" "figwheel.main" "-co" "test.cljs.edn" "-m" "same.test-runner"]
